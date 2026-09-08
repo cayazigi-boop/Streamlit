8b799f1debc1e83485f657a048667ba3f3d02c18
carlosyazigi <231666281+cayazigi-boop@users.noreply.github.com>
2026-09-08 17:48:46 -0300

    Atualiza app.py

--- app.py
+++ app.py
@@ -1,83 +1,82 @@
-```python
 import pandas as pd
 import scipy.stats
 import streamlit as st
 import time
 
-# Variáveis persistentes preservadas enquanto o Streamlit executa novamente o script
+# Variáveis persistentes
+
 if 'experiment_no' not in st.session_state:
-    st.session_state['experiment_no'] = 0
+st.session_state['experiment_no'] = 0
 
 if 'df_experiment_results' not in st.session_state:
-    st.session_state['df_experiment_results'] = pd.DataFrame(
-        columns=['no', 'iterations', 'mean']
-    )
+st.session_state['df_experiment_results'] = pd.DataFrame(
+columns=['no', 'iterations', 'mean']
+)
 
 st.header('Jogando uma moeda')
 
 number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
 start_button = st.button('Executar')
 
-
 def toss_coin(n, chart):
 
-    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
-    outcome_no = 0
-    outcome_1_count = 0
-    means = []
+trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
-    for r in trial_outcomes:
-        outcome_no += 1
+outcome_no = 0
+outcome_1_count = 0
+means = []
 
-        if r == 1:
-            outcome_1_count += 1
+for r in trial_outcomes:
+    outcome_no += 1
 
-        mean = outcome_1_count / outcome_no
-        means.append(mean)
+    if r == 1:
+        outcome_1_count += 1
 
-        # Atualiza o gráfico
-        chart.line_chart(pd.DataFrame({
-            'Média': means
-        }))
+    mean = outcome_1_count / outcome_no
+    means.append(mean)
 
-        time.sleep(0.05)
+    chart.line_chart(
+        pd.DataFrame({'Média': means})
+    )
 
-    return mean
+    time.sleep(0.05)
+
+return mean
 
 
 if start_button:
 
-    st.write(
-        f'Executando o experimento de {number_of_trials} tentativas.'
-    )
 
-    st.session_state['experiment_no'] += 1
+st.write(
+    f'Executando o experimento de {number_of_trials} tentativas.'
+)
 
-    # Cria espaço para o gráfico
-    chart = st.empty()
+st.session_state['experiment_no'] += 1
 
-    mean = toss_coin(number_of_trials, chart)
+chart = st.empty()
 
-    st.session_state['df_experiment_results'] = pd.concat(
-        [
-            st.session_state['df_experiment_results'],
-            pd.DataFrame(
-                data=[[
-                    st.session_state['experiment_no'],
-                    number_of_trials,
-                    mean
-                ]],
-                columns=['no', 'iterations', 'mean']
-            )
-        ],
-        axis=0
-    )
+mean = toss_coin(number_of_trials, chart)
 
-    st.session_state['df_experiment_results'] = (
-        st.session_state['df_experiment_results']
-        .reset_index(drop=True)
-    )
+st.session_state['df_experiment_results'] = pd.concat(
+    [
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(
+            data=[[
+                st.session_state['experiment_no'],
+                number_of_trials,
+                mean
+            ]],
+            columns=['no', 'iterations', 'mean']
+        )
+    ],
+    axis=0
+)
+
+st.session_state['df_experiment_results'] = (
+    st.session_state['df_experiment_results']
+    .reset_index(drop=True)
+)
 
 
 st.write(st.session_state['df_experiment_results'])
